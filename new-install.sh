34320d288ccfea272e3e5c6e0d6c19f8ec135dc4
Myyk Seok <myyk.seok@gmail.com>
2018-07-02 17:58:44 +0800

    add a .gitignore and a new install script and a simple ssh config.

---
+++ new-install.sh
@@ -0,0 +1,5 @@
+echo "Installing brew and stuff that installs with brew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install homebrew/cask/keepingyouawake
+brew install homebrew/cask/atom
+brew install git
